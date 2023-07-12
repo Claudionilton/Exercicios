ec0bcb0a3c3b962b394e5982ec84bfd29a0f31c2
Claudionilton <claudioniltonjose@gmail.com>
2023-07-12 16:15:03 -0300

    Exercicio 02

--- Exercicio01/src/Main.java
+++ Exercicio01/src/Main.java
@@ -10,13 +10,16 @@ public class Main {
         float numero1;
         float numero2;
 
-        System.out.println("Soma de numeros ");
+        System.out.println("Operações matematicas ");
         System.out.println("Digite o primeiro numero: ");
         numero1 = leitor.nextFloat();
         System.out.println("Digite o segundo numero: ");
         numero2 = leitor.nextFloat();
 
         System.out.println("A soma dos numeros é: " + (numero1+numero2));
+        System.out.println("A subtração dos numeros é: " + (numero1-numero2));
+        System.out.println("A multiplicação dos numeros é: " + (numero1*numero2));
+        System.out.println("A divisao numeros é: " + (numero1/numero2));
 
     }
 }
