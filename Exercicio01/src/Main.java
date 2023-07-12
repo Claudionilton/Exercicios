9d9dcc67187f38230256c2a6adfdf969e33ffb21
Claudionilton <claudioniltonjose@gmail.com>
2023-07-12 16:06:47 -0300

    exercicio de soma

---
+++ Exercicio01/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+/**
+    Faça um algoritmo que receba dois números e exiba o resultado da sua soma;
+ **/
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
+
+        float numero1;
+        float numero2;
+
+        System.out.println("Soma de numeros ");
+        System.out.println("Digite o primeiro numero: ");
+        numero1 = leitor.nextFloat();
+        System.out.println("Digite o segundo numero: ");
+        numero2 = leitor.nextFloat();
+
+        System.out.println("A soma dos numeros é: " + (numero1+numero2));
+
+    }
+}
